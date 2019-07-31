8b4412c39571e14d9549d22aae9785dbed51082f
Marcos Ferreira <marcosradix@gmail.com>
2019-07-30 21:39:34 -0300

    atualizando para pegar exception de usuário já existente

--- src/main/java/br/com/workmade/exception/resourceAdvice/RestResponseEntityExceptionHandler.java
+++ src/main/java/br/com/workmade/exception/resourceAdvice/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.workmade.exception.resourceAdvice;
 
+import br.com.workmade.exception.ObjectAlreadyExistsException;
 import br.com.workmade.exception.ObjectNotFoundException;
 import br.com.workmade.exception.StandardError;
 import org.springframework.http.HttpStatus;
@@ -22,5 +23,11 @@ public class RestResponseEntityExceptionHandler {
     public ResponseEntity<StandardError> usernameNotFoundException(UsernameNotFoundException object, HttpServletRequest request){
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Usuário não encontrado",object.getMessage(),request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(ObjectAlreadyExistsException.class)
+    public ResponseEntity<StandardError> objectAlreadyExistsException(ObjectAlreadyExistsException object, HttpServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Usuário não encontrado",object.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
